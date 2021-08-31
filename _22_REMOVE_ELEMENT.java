d70f30ade3db43fa584895cc793a43c1e5d2726f
Tarrykege <xoiiwkingg@gmail.com>
2021-08-31 09:57:36 +0700

    id_22_done

---
+++ _22_REMOVE_ELEMENT.java
@@ -0,0 +1,24 @@
+class _22_REMOVE_ELEMENT  {
+    public static int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int i=0;
+        
+        while(i<n){
+            if(nums[i]==val){
+                int j=i;
+
+                while(j<n-1){
+                    nums[j]=nums[j+1];
+                    j++;
+                }
+                n--;
+            } else i++;
+        }
+        return n;
+    }
+    public static void main(String[] args) {
+        int[] n={0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(n,2));
+        System.out.println("done");
+    }
+}
