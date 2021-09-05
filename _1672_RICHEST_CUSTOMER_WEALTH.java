937fa52c9239cfb515ca98aaaad0bcbc3ba22f91
Tarrykege <xoiiwkingg@gmail.com>
2021-09-05 19:37:45 +0700

    id_1672_DONE

---
+++ _1672_RICHEST_CUSTOMER_WEALTH.java
@@ -0,0 +1,16 @@
+class _1672_RICHEST_CUSTOMER_WEALTH {
+    public static int maximumWealth(int[][] a) {
+        int row= a.length;
+        int col= a[0].length;
+        int max=0;
+        int wealth=0;
+        for (int i = 0; i < row; i++) {
+            wealth=0;
+            for (int j = 0; j < col; j++) {
+                wealth+=a[i][j];
+            }
+            if(max<wealth) max=wealth;
+        }
+        return max;
+    }
+}
