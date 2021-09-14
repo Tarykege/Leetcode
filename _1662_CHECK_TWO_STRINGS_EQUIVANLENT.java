b49c71b104ec95a7e18c975c0b168282b2fe1dff
Tarykege <xoiiwkingg@gmail.com>
2021-09-14 08:16:20 +0700

    id_1662_StringBuilder_DONE

---
+++ _1662_CHECK_TWO_STRINGS_EQUIVANLENT.java
@@ -0,0 +1,18 @@
+public class _1662_CHECK_TWO_STRINGS_EQUIVANLENT{
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder r1= new StringBuilder();
+        StringBuilder r2= new StringBuilder();
+        for (String s : word1) {
+            r1.append(s);
+        }
+        for (String s : word2) {
+            r2.append(s);
+        }
+        return r1.toString().equals(r2.toString());
+    }
+    public static void main(String[] args) {
+        String[] s1= {"abc","d"};
+        String[] s2= {"a", "bc", "d"};
+        System.out.println(arrayStringsAreEqual(s1,s2));
+    }
+}
