86fc2328b35d620658bd4347d11b6b150acf986f
Tarykege <xoiiwkingg@gmail.com>
2021-10-04 08:18:25 +0700

    add_id_55_JUMP_GAME

---
+++ _55_JUMP_GAME.java
@@ -0,0 +1,21 @@
+/**
+ * _55_JUMP_GAME
+ * tim buoc nhay cao nhat trong cac index 
+ */
+public class _55_JUMP_GAME {
+    public static boolean canJump(int[] nums){
+        int n=nums.length;
+        int maxPath=0;
+        for(int i=0; i<n; i++){
+            maxPath=Math.max(maxPath,i+nums[i]);
+            if(maxPath>=n-1) return true;
+            else if(maxPath<i+1) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] a={0};
+        System.out.println(canJump(a));
+    }
+    
+}
