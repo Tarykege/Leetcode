768611e231dddc314d74a6c476cd672e79bc1994
Tarykege <xoiiwkingg@gmail.com>
2021-10-06 08:12:45 +0700

    add_id_278_first_bad_version

---
+++ _278_FIRST_BAD_VERSION.java
@@ -0,0 +1,20 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+public class _278_FIRST_BAD_VERSION {
+    public static boolean isBadVersion (int version){
+        return true;
+    }
+    public static int firstBadVersion(int n){
+        int left=1,right=n;
+        
+        while(left!=right){
+            int mid=left+(right-left)/2;
+            if(!isBadVersion(mid)){
+                left=mid+1;
+            }else {
+                right=mid;
+            }
+        }
+        return right;
+    }
+}
