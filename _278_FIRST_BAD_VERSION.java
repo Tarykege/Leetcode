c5b4e8ab6765878fcfb60169572b0e69e27798cf
Tarykege <xoiiwkingg@gmail.com>
2021-10-06 08:13:54 +0700

    fix

--- _278_FIRST_BAD_VERSION.java
+++ _278_FIRST_BAD_VERSION.java
@@ -1,12 +1,13 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
+//use binary search
 public class _278_FIRST_BAD_VERSION {
     public static boolean isBadVersion (int version){
         return true;
     }
     public static int firstBadVersion(int n){
         int left=1,right=n;
-        
+
         while(left!=right){
             int mid=left+(right-left)/2;
             if(!isBadVersion(mid)){
