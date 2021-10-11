be9b00d385860b1a7318e4a98cbadf9b949cba70
Tarykege <xoiiwkingg@gmail.com>
2021-10-11 20:15:06 +0700

    first_commit_id_19

---
+++ _19_REMOVE_NTH_NODE_FROM_END.java
@@ -0,0 +1,32 @@
+/**
+ * _19._REMOVE_NTH_NODE_FROM_END
+ */
+public class _19_REMOVE_NTH_NODE_FROM_END {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode[] A = new ListNode[100];
+        int t = 0;
+        while (head != null) {
+            A[t++] = head;
+            head = head.next;
+        }
+        return A[t / 2];
+    }
+    
+}
