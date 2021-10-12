23fe9fbd88f32bcb48b085491cc58b7e3cecf405
Tarykege <xoiiwkingg@gmail.com>
2021-10-12 15:38:05 +0700

    first_commit

---
+++ _3_LONGEST_SUBTRING_WITHOUT_REPEATING_CHARACTERS.java
@@ -0,0 +1,13 @@
+public class _3_LONGEST_SUBTRING_WITHOUT_REPEATING_CHARACTERS {
+    public static int lengthOfLongestSubstring(String s) {
+        int[] a = new int[123];
+        int countMax = 0, countCur = 0;
+
+        return countMax;
+    }
+
+    public static void main(String[] args) {
+        String s = " ";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
---
+++ _567_PERMUTATION_IN_STRING.java
@@ -0,0 +1,12 @@
+public class _567_PERMUTATION_IN_STRING {
+    public static boolean checkInclusion(String s1, String s2) {
+        boolean check = true;
+        
+        return check;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab", s2 = "eidboaoo";
+        System.out.println(checkInclusion(s1, s2));
+    }
+}
